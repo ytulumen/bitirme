f884820ee6bac6f82586472af30458dbaa19a58f
ytulumen <yasintulumen@gtu.edu.tr>
2018-06-06 00:53:59 +0300

    everything done

---
+++ createelection.sql
@@ -0,0 +1,12 @@
+INSERT INTO `votingsystem`.`election`
+(`electionid`,`topic`,`title`,`isVotable`)
+VALUES
+(0001,'Cars','Select most beautiful car',1);
+INSERT INTO `votingsystem`.`election`
+(`electionid`,`topic`,`title`,`isVotable`)
+VALUES
+(0002,'OSs','Select best OS',1);
+INSERT INTO `votingsystem`.`election`
+(`electionid`,`topic`,`title`,`isVotable`)
+VALUES
+(0003,'Smart Phones','Select most useful smart phone',1);
